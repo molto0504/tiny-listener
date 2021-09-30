06bbd0057e8bd5acd5b4b8ff0d64d0eda471f73b
molto <wy6269@gmail.com>
2021-09-30 17:56:50 +0800

    feat: add examples

---
+++ examples/event_in_order.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from pyevent.application import PyEvent, Event, Context
+
+
+class App(PyEvent):
+    async def listen(self, send):
+        Context(cid="1").jobs.add("job.1", "job.2")
+        send(Event("event.step_3", cid="1"))
+        send(Event("event.step_2", cid="1"))
+        send(Event("event.step_1", cid="1"))
+        await asyncio.sleep(1)
+
+
+app = App()
+
+
+@app.event("event.step_1")
+async def do_something(ctx: Context):
+    print("Step 1!")
+    ctx.jobs.done("job.1")
+
+
+@app.event("event.step_2", must_done=["job.1"])
+async def do_something(ctx: Context):
+    print("Step 2!")
+    ctx.jobs.done("job.2")
+
+
+@app.event("event.step_3", must_done=["job.2"])
+async def do_something(ctx: Context):
+    print("Step 3!")
+    ctx.jobs.done("job.3")
+
+
+app.run()
---
+++ examples/helloword.py
@@ -0,0 +1,29 @@
+from pyevent.application import PyEvent, Event
+
+
+class App(PyEvent):
+    async def listen(self, send):
+        send(Event("event.foo"))
+        send(Event("event.bar"))
+        send(Event("event.baz"))
+
+
+app = App()
+
+
+@app.event("event.foo")
+async def do_something():
+    print("Hello foo!")
+
+
+@app.event("event.bar")
+async def do_something():
+    print("Hello bar!")
+
+
+@app.event("event.baz")
+async def do_something():
+    print("Hello baz!")
+
+
+app.run()
