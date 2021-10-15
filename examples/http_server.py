62b6f9efc1fa40185a233d3d4a986f7b14ae50d7
molto <wy6269@gmail.com>
2021-10-15 13:28:37 +0800

    feat: add examples http server

---
+++ examples/http_server.py
@@ -0,0 +1,29 @@
+import asyncio
+from typing import Callable
+
+from tiny_listener import Listener, Event
+
+
+class App(Listener):
+    async def handler(self, req: asyncio.StreamReader, resp: asyncio.StreamWriter) -> None:
+        line: bytes = await req.readline()
+        if line:
+            _, path, *_ = line.decode().split()
+            await self.todo(path, resp=resp, block=True)
+
+    async def listen(self, todo: Callable[..., None]):
+        await asyncio.start_server(self.handler, host="0.0.0.0", port=8080, loop=self.loop)
+        print(f"Run http server on :8080")
+
+
+app = App()
+
+
+@app.do("/")
+async def http_api(event: Event):
+    resp = event.detail["resp"]
+    resp.write(b"HTTP/1.1 200 OK\nContent-Length: 13\n\nHello, world!")
+    resp.close()
+
+
+app.run(forever=True)
