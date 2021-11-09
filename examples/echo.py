382aa679ef72bca44ff1aa8e5019d073bd572665
molto <wy6269@gmail.com>
2021-11-09 19:48:46 +0800

    fix: examples

--- examples/echo.py
+++ examples/echo.py
@@ -1,41 +1,29 @@
 import asyncio
 
-from tiny_listener import Listener, Context
-from concurrent.futures import TimeoutError
+from tiny_listener import Listener, Params
 
 
 class App(Listener):
     async def listen(self, todo):
-        todo("step_1", cid="Alice")
-        todo("step_3", cid="Bob")
-        todo("step_2", cid="Bob", timeout=1)
-        todo("step_3", cid="Alice")
-        todo("step_1", cid="Bob")
-        todo("step_2", cid="Alice")
+        idx = 0
+        while True:
+            line = input(f"{idx}, please enter: ")
+            todo(line)
+            idx += 1
+            await asyncio.sleep(0)
 
 
 app = App()
 
-
-@app.error_raise(TimeoutError)
-async def err_handler(err: TimeoutError):
-    print(err)
+lines = []
 
 
-@app.do("step_1")
-async def something(ctx: Context):
-    print(f"* step_1 | {ctx.cid}")
+@app.do("done")
+async def something():
+    print(" ".join(lines))
+    app.exit()
 
 
-@app.do("step_2", parents=["step_1"])
-async def something(ctx: Context):
-    await asyncio.sleep(3)
-    print(f"* step_2 | {ctx.cid}")
-
-
-@app.do("step_3", parents=["step_2"])
-async def something(ctx: Context):
-    print(f"* step_3 | {ctx.cid}")
-
-
-app.run()
+@app.do("{line}")
+async def something(param: Params):
+    lines.append(param["line"])
