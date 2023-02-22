ca051877dd5ac8c4ca7d44c0ee2e8efbdf840c76
molto <wy6269@gmail.com>
2023-02-22 09:44:38 +0800

    feat: test echo report path

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,3 +4,4 @@ set -e
 set -x
 
 pytest --cov-config=.coveragerc --cov=tiny_listener --cov-report=term-missing:skip-covered --cov-report=html --cov-report=xml tests "${@}"
+echo "report: $(pwd)/htmlcov/index.html"
