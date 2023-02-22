73e191298c9a38f5a0913936d5e8d6dacee35b12
molto <wy6269@gmail.com>
2023-02-22 11:46:36 +0800

    enh: lint.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+# pip install pytest==6.2.5 pytest-asyncio==0.16.0 pytest-cov==3.0.0
+
 set -e
 set -x
 
-pytest --cov-config=.coveragerc --cov=tiny_listener --cov-report=term-missing:skip-covered --cov-report=html --cov-report=xml tests "${@}"
+target=${*}
+if [ -z "${target}" ]; then
+    target=( "tests" )
+fi
+
+pytest \
+--cov-config=.coveragerc \
+--cov=tiny_listener \
+--cov-report=term-missing:skip-covered \
+--cov-report=html \
+--cov-report=xml \
+"${target[@]}"
 echo "report: $(pwd)/htmlcov/index.html"
