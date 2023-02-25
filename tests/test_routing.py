642c9be1b87cb246df7813d1c5774b428df46e50
molto <wy6269@gmail.com>
2023-02-26 00:23:36 +0800

    refactor: routing unittest

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -1,9 +1,83 @@
 import uuid
-from unittest import TestCase
+from typing import Callable, Awaitable
 
 import pytest
 
 from tiny_listener.routing import CONVERTOR_TYPES, Route, RouteError, compile_path
+
+
+@pytest.fixture
+def handler() -> Callable[..., Awaitable[None]]:
+    async def mock_handler() -> None:
+        return
+
+    return mock_handler
+
+
+def test_route(handler):
+    route = Route(
+        name="/user/bob",
+        fn=handler,
+        after=["/user/{name}", handler],
+        opts={"foo": "bar"},
+    )
+    assert route.path == "/user/bob"
+    assert route.convertors == {}
+    assert route.opts == {"foo": "bar"}
+    assert route.match("/user/bob") == {}
+    assert route.match("/user") is None
+    assert route.after == ["/user/{name}", "mock_handler"]
+
+
+@pytest.mark.parametrize('name', ["/user/{name}", "/user/{name:str}"])
+def test_convertor_str(handler, name) -> None:
+    route = Route(name=name, fn=handler)
+    assert route.match("/user/bob") == {"name": "bob"}
+    assert route.match("/user") is None
+
+
+def test_convertor_int(handler):
+    route = Route(name="/user/{age:int}", fn=handler)
+    assert route.match("/user/18") == {"age": 18}
+    assert route.match("/user") is None
+
+
+def test_convertor_float(handler):
+    route = Route(name="/user/{score:float}", fn=handler)
+    assert route.match("/user/1.1") == {"score": 1.1}
+    assert route.match("/user") is None
+
+
+def test_convertor_path(handler):
+    route = Route(name="/user/{file:path}", fn=handler)
+    assert route.match("/user/document/repo/foo.py") == {"file": "document/repo/foo.py"}
+    assert route.match("/user/http://localhost/home") == {"file": "http://localhost/home"}
+    assert route.match("/user") is None
+
+
+def test_convertor_uuid(handler):
+    route = Route(name="/user/{id:uuid}", fn=handler)
+    assert route.match("/user/18baadd0-9225-4cc0-a13b-69098168689f") == {"id": uuid.UUID("18baadd0-9225-4cc0-a13b-69098168689f")}
+    assert route.match("/user") is None
+
+
+def test_convertor_complex(handler):
+    route = Route(
+        name="/user/{id:uuid}/{name}/{file:path}/{age:int}/{score:float}",
+        fn=handler,
+    )
+    assert route.match("/user/18baadd0-9225-4cc0-a13b-69098168689f/bob/document/repo/foo.py/18/1.1") == {
+        "id": uuid.UUID("18baadd0-9225-4cc0-a13b-69098168689f"),
+        "name": "bob",
+        "file": "document/repo/foo.py",
+        "age": 18,
+        "score": 1.1,
+    }
+
+
+def test_convertor_not_exist(handler):
+    with pytest.raises(RouteError):
+        Route(name="/user/{name:int128}", fn=handler)
 
 
 def test_compile_path() -> None:
@@ -13,81 +87,3 @@ def test_compile_path() -> None:
     _, convertors = compile_path("/user/{age:int}")
     assert "age" in convertors
     assert convertors["age"] is CONVERTOR_TYPES["int"]
-
-
-class TestRoute(TestCase):
-    def setUp(self) -> None:
-        async def f() -> None:
-            return
-
-        self.handler = f
-
-    def test_ok(self) -> None:
-        route = Route(
-            name="/user/bob",
-            fn=self.handler,
-            after="/user/{name}",
-            opts={"foo": "bar"},
-        )
-        self.assertEqual("/user/bob", route.path)
-        self.assertEqual({}, route.convertors)
-        self.assertEqual({"foo": "bar"}, route.opts)
-        self.assertEqual({}, route.match("/user/bob"))
-        self.assertIsNone(route.match("/user"))
-        self.assertEqual(["/user/{name}"], route.after)
-
-    def test_convertor_str(self) -> None:
-        route = Route(name="/user/{name}", fn=self.handler)
-        self.assertEqual({"name": "bob"}, route.match("/user/bob"))
-        self.assertIsNone(route.match("/user"))
-
-        route = Route(name="/user/{name:str}", fn=self.handler)
-        self.assertEqual({"name": "bob"}, route.match("/user/bob"))
-        self.assertIsNone(route.match("/user"))
-
-    def test_convertor_int(self) -> None:
-        route = Route(name="/user/{age:int}", fn=self.handler)
-        self.assertEqual({"age": 18}, route.match("/user/18"))
-        self.assertIsNone(route.match("/user"))
-
-    def test_convertor_float(self) -> None:
-        route = Route(name="/user/{score:float}", fn=self.handler)
-        self.assertEqual({"score": 1.1}, route.match("/user/1.1"))
-        self.assertIsNone(route.match("/user"))
-
-    def test_convertor_path(self) -> None:
-        route = Route(name="/user/{file:path}", fn=self.handler)
-        self.assertEqual({"file": "document/repo/foo.py"}, route.match("/user/document/repo/foo.py"))
-        self.assertEqual(
-            {"file": "http://localhost/home"},
-            route.match("/user/http://localhost/home"),
-        )
-        self.assertIsNone(route.match("/user"))
-
-    def test_convertor_uuid(self) -> None:
-        route = Route(name="/user/{id:uuid}", fn=self.handler)
-        self.assertEqual(
-            {"id": uuid.UUID("18baadd0-9225-4cc0-a13b-69098168689f")},
-            route.match("/user/18baadd0-9225-4cc0-a13b-69098168689f"),
-        )
-        self.assertIsNone(route.match("/user"))
-
-    def test_convertor_complex(self) -> None:
-        route = Route(
-            name="/user/{id:uuid}/{name}/{file:path}/{age:int}/{score:float}",
-            fn=self.handler,
-        )
-        self.assertEqual(
-            route.match("/user/18baadd0-9225-4cc0-a13b-69098168689f/bob/document/repo/foo.py/18/1.1"),
-            {
-                "id": uuid.UUID("18baadd0-9225-4cc0-a13b-69098168689f"),
-                "name": "bob",
-                "file": "document/repo/foo.py",
-                "age": 18,
-                "score": 1.1,
-            },
-        )
-
-    def test_convertor_not_exist(self) -> None:
-        with pytest.raises(RouteError):
-            Route(name="/user/{name:int128}", fn=self.handler)
