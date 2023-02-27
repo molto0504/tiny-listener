8ea16fafb26cae8f1cdfcd6cdcbb63a1ee600898
molto <wy6269@gmail.com>
2023-02-27 16:44:56 +0800

    fix: rabbitmq example on close

--- examples/rabbitmq_consumer.py
+++ examples/rabbitmq_consumer.py
@@ -5,6 +5,7 @@ Before running this example, you need to install RabbitMQ and start it.
 
 See: https://molto0504.github.io/tiny-listener/usage-rabbitmq-consumer/
 """
+import asyncio
 
 import aio_pika
 
@@ -12,13 +13,16 @@ from tiny_listener import Event, Listener
 
 
 class App(Listener):
-    async def listen(self):
-        connection = await aio_pika.connect_robust("amqp://127.0.0.1/")
-        async with connection:
-            channel = await connection.channel()
-            queue = await channel.declare_queue("test_queue", auto_delete=True)
-            app.trigger_event("/produce", data={"channel": channel})
+    def __init__(self):
+        super().__init__()
+        self.conn = None
 
+    async def listen(self):
+        self.conn = await aio_pika.connect_robust("amqp://127.0.0.1/")
+        async with self.conn:
+            channel = await self.conn.channel()
+            queue = await channel.declare_queue("test_queue", auto_delete=True)
+            app.trigger_event("/mock_producer", data={"channel": channel})
             async with queue.iterator() as msg_queue:
                 async for msg in msg_queue:
                     async with msg.process():
@@ -28,15 +32,17 @@ class App(Listener):
 app = App()
 
 
-@app.on_event("/produce")
+@app.shutdown
+async def shutdown():
+    await app.conn.close()
+
+
+@app.on_event("/mock_producer")
 async def produce(event: Event):
     channel = event.data["channel"]
-    await channel.default_exchange.publish(
-        aio_pika.Message(body=b"Hello, Alice!", app_id="001"), routing_key="test_queue"
-    )
-    await channel.default_exchange.publish(
-        aio_pika.Message(body=b"Hello, Bob!", app_id="002"), routing_key="test_queue"
-    )
+    for i in range(10):
+        await channel.default_exchange.publish(aio_pika.Message(body=bytes(i), app_id=str(i)), routing_key="test_queue")
+        await asyncio.sleep(1)
 
 
 @app.on_event("/app/{app_id}/consume")
